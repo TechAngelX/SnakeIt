a87b0f08a7ef9a8af8f2fb251871818a03eea7e6
Ricki Angel <rickihippy@hotmail.com>
2024-04-14 09:08:33 +0100

    GamePanel: Declared game parameters

--- src/main/java/org/snakeIt/GamePanel.java
+++ src/main/java/org/snakeIt/GamePanel.java
@@ -2,12 +2,29 @@ package org.snakeIt;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
+import java.util.Random;
+import javax.swing.*;
+
 
 public class GamePanel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT / UNIT_SIZE);
+    static final int DELAY = 75; // THe higher the number, the slower the game, and vice versa.
+
+    final int X[] = new int[GAME_UNITS];
+    final int y[] = new int [GAME_UNITS];
+    int bodyParts = 6; // The body parts on the snake.
+    int applesEaten = 0;
+    int appleX; // The X co-ordinate of where the apple is placed. will appear randomly each time snake eats apple.
+    int appleY; // Ditto.
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
     GamePanel() {
 
